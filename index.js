f8613ba84364633d28b35aa0a46106e0c48a6b9b
Fida Muhammad Khan <khannfid@corp.contoursoftware.net>
2023-04-10 12:51:18 +0500

    Calculator

---
+++ index.js
@@ -0,0 +1,51 @@
+let a= "";
+let numb3="";
+let op="";
+let b="";
+function numbers(num)
+{
+    a= a + num;
+    document.getElementById('numb').value=a;
+    function numbers(num) {
+        if (op === '') { // First number
+            a = a + num;
+        } else { // Second number
+            a = num;
+            op = ''; // Reset operator
+        }
+        document.getElementById('numb').value = a;
+    }
+}
+
+function operator(ope)
+{
+    op= ope;
+    document.getElementById('numb').value=op;
+    b=parseFloat(a);
+    a="";
+}
+function equall()
+{
+    if(op === '+')
+{
+    numb3= b + parseFloat (a);
+}
+else if(op === '-')
+{
+    numb3= b-  parseFloat (a);
+}
+else if(op === '*')
+{
+    numb3= b * parseFloat (a);
+}
+else if(op === '/')
+{
+    numb3=b / parseFloat (a);
+}
+a = numb3.toString();
+    document.getElementById('numb').value= numb3 
+}
+function clearInput()
+{
+    document.getElementById('numb').value= "0"
+}
